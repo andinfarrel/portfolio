b0e6c96eb177be622c06f5c9e70c235bad38f939
andinfarrel <40709093+andinfarrel@users.noreply.github.com>
2020-08-30 21:04:12 +0100

    minor changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '3c$-7lg_^$f!i$jm9--gcgy4#!7qx64m1w3_b3qeigk)-yvo^*'
 with open('./portfolio/secret.txt') as f:
     SECRET_KEY = f.read().strip()
 
